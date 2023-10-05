4429e4f0b928102599a1a81764593ee782daf158
Claudemir-23 <146127439+Claudemir-23@users.noreply.github.com>
2023-10-05 16:58:05 -0300

    atualizei

--- javaScript3/ex02.js
+++ javaScript3/ex02.js
@@ -1,2 +1,7 @@
-var vel =60
-    console.log(' A velo é, ' , $,{vel} 
+var vel = 56
+console.log(`A velociddade do seu carro é ,${vel}`)
+if (vel > 60) {
+    console.log("Voçe ultrapassou a velocidade. Multado")
+}
+
+console.log('Dirija sempre usando cinto de segurança')
