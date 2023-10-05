beca888656241faa4ebdd1c26f1fabca5e17dff8
Claudemir-23 <146127439+Claudemir-23@users.noreply.github.com>
2023-10-05 14:38:08 -0300

    ajustes no js

---
+++ javaScript3/ex01.js
@@ -0,0 +1,11 @@
+var n = 18;
+//console.log('claudemir mendonça')
+console.log('òla mundo')
+console.log(17+ 23+35)
+console.log('chegarei em casa ás 10 horas da noite');
+console.log('Meu nome é "Claudemir".Estou aprendendo JavaScript às 10 da manhã.')
+//comentário de linha
+/*
+comentario de bloco
+de linha
+ */
